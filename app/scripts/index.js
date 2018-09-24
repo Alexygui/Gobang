40c32ee7ce9bfd4bfa1e97e24ee88bae7f515739
alexygui <guigen.wang.cn@gmail.com>
2018-09-24 23:57:15 +0800

    修改前端
    修改前端

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,5 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import '../styles/app.css'
+import '../css/style.css'
 
 // Import libraries we need.
 import { default as Web3 } from 'web3'
@@ -14,33 +14,70 @@ const Gobang = contract(gobangArtifact)
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
-let accounts
-let account
+
+let canvas = document.getElementById('chess')
+let context = canvas.getContext('2d')
+let me = true // 判断该轮黑白棋落子权
+let over = false // 判断游戏是否结束
+let chessBoard = [] // 棋盘二维数组,存储棋盘信息
+let isPlaying = false // 是否正在进行游戏
+let timeout // 间隔请求状态的时间
 
 const App = {
   start: function () {
-    const self = this
+    // 初始化棋盘信息
+    for (var i = 0; i < 15; i++) {
+      chessBoard[i] = []
+      for (var j = 0; j < 15; j++) {
+        chessBoard[i][j] = 0
+      }
+    }
 
     // Bootstrap the Gobang abstraction for Use.
     Gobang.setProvider(web3.currentProvider)
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
-
-      if (accs.length === 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
-        return
-      }
-
-      accounts = accs
-      account = accounts[0]
-
-      self.refreshBalance()
+    Gobang.deployed().then(function (instance) {
+      window.gobang = instance
+      console.log(window.gobang)
     })
+  },
+
+  joinGame: function () {
+    let self = this
+
+    window.gobang.joinGame({
+      from: web3.eth.accounts[0]
+    }).then(function (re) {
+      self.cleanChess()
+      self.drawChess()
+      self.setStatus('Transaction complete!')
+      console.log('joinGame:' + re)
+
+      timeout = setInterval(function () {
+        self.getNewestState()
+      }, 3000)
+    })
+      .catch(function (e) {
+        console.log(e)
+      // self.setStatus('Error sending coin; see log.')
+      })
+  },
+
+  getNewestState: function () {
+    window.gobang.getNewestState({ from: web3.eth.accounts[0] })
+      .then(function (re) {
+        console.log('getNewestState:' + re)
+        if (!isPlaying) {
+        // 清除循环执行
+          clearInterval(timeout)
+        }
+      }).then(function (data) {
+        console.log('getNewestState:' + data)
+      // self.setStatus('Transaction complete!')
+      }).catch(function (e) {
+        console.log(e)
+      // self.setStatus('Error sending coin; see log.')
+      })
   },
 
   setStatus: function (message) {
@@ -48,41 +85,54 @@ const App = {
     status.innerHTML = message
   },
 
-  refreshBalance: function () {
-    const self = this
-
-    let meta
-    Gobang.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
-    }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
-    })
+  /**
+     * 清除棋盘
+     */
+  cleanChess: function () {
+    context.fillStyle = '#FFFFFF'
+    context.fillRect(0, 0, canvas.width, canvas.height)
   },
 
-  sendCoin: function () {
-    const self = this
+  /**
+     * 绘制棋盘
+     */
+  drawChess: function () {
+    for (let i = 0; i < 15; i++) {
+      context.strokeStyle = '#BFBFBF'
+      context.beginPath()
+      context.moveTo(15 + i * 30, 15)
+      context.lineTo(15 + i * 30, canvas.height - 15)
+      context.closePath()
+      context.stroke()
+      context.beginPath()
+      context.moveTo(15, 15 + i * 30)
+      context.lineTo(canvas.width - 15, 15 + i * 30)
+      context.closePath()
+      context.stroke()
+    }
+  },
 
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-
-    this.setStatus('Initiating transaction... (please wait)')
-
-    let meta
-    Gobang.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
-    }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
-    })
+  /**
+     * 绘制棋子
+     * @param i     棋子x轴位置
+     * @param j     棋子y轴位置
+     * @param me    棋子颜色
+     */
+  oneStep: function (i, j, me) {
+    context.beginPath()
+    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI)
+    context.closePath()
+    let gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2,
+      13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0)
+    if (me) {
+      gradient.addColorStop(0, '#D1D1D1')
+      gradient.addColorStop(1, '#F9F9F9')
+    } else {
+      gradient.addColorStop(0, '#0A0A0A')
+      gradient.addColorStop(1, '#636766')
+    }
+    context.fillStyle = gradient
+    context.fill()
   }
 }
 
@@ -93,20 +143,20 @@ window.addEventListener('load', function () {
   if (typeof web3 !== 'undefined') {
     console.warn(
       'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 Gobang,' +
-      ' ensure you\'ve configured that source properly.' +
-      ' If using MetaMask, see the following link.' +
-      ' Feel free to delete this warning. :)' +
-      ' http://truffleframework.com/tutorials/truffle-and-metamask'
+            ' If you find that your accounts don\'t appear or you have 0 Gobang,' +
+            ' ensure you\'ve configured that source properly.' +
+            ' If using MetaMask, see the following link.' +
+            ' Feel free to delete this warning. :)' +
+            ' http://truffleframework.com/tutorials/truffle-and-metamask'
     )
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider)
   } else {
     console.warn(
       'No web3 detected. Falling back to http://127.0.0.1:9545.' +
-      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-      ' Consider switching to Metamask for development.' +
-      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+            ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+            ' Consider switching to Metamask for development.' +
+            ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
     )
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
@@ -114,3 +164,26 @@ window.addEventListener('load', function () {
 
   App.start()
 })
+
+/**
+ * canvas 鼠标点击事件
+ * @param e
+ */
+canvas.onclick = function (e) {
+  if (over) {
+    return
+  }
+
+  let x = e.offsetX
+  let y = e.offsetY
+  let i = Math.floor(x / 30)
+  let j = Math.floor(y / 30)
+
+  // 如果该位置没有棋子,则允许落子
+  if (chessBoard[i][j] === 0) {
+    // 绘制棋子(玩家)
+    App.oneStep(i, j, me)
+    // 改变棋盘信息(该位置有棋子)
+    chessBoard[i][j] = 1
+  }
+}
--- contracts/Gobang.sol
+++ contracts/Gobang.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.17;
 
+
 contract Gobang {
     address owner;
 
@@ -8,120 +9,138 @@ contract Gobang {
     int8 lastX;
     int8 lastY;
     }
+
     Player playerBlack;
+
     Player playerWhite;
+
     uint128 BET_MONEY = 1 ether;
+
     bool isBlackPlay = true;
+
+    bool isPlaying = false;
+
     uint8[15][15] chessboard;
+
     address winner;
-    mapping(address => uint128) playersMoney;
+
+    mapping (address => uint128) playersMoney;
     // 三维数组记录横向，纵向，左斜，右斜的移动
     int8[2][2][4] dir = [
-    [[int8(-1), int8(0)], [int8(1), int8(0)]],            // 横向
-    [[int8(0), int8(-1)], [int8(0), int8(1)]],            // 竖着
-    [[int8(-1), int8(-1)], [int8(1), int8(1)]],            // 左斜
-    [[int8(1), int8(-1)], [int8(-1), int8(1)]]            // 右斜
+    [[int8(- 1), int8(0)], [int8(1), int8(0)]], // 横向
+    [[int8(0), int8(- 1)], [int8(0), int8(1)]], // 竖着
+    [[int8(- 1), int8(- 1)], [int8(1), int8(1)]], // 左斜
+    [[int8(1), int8(- 1)], [int8(- 1), int8(1)]]            // 右斜
     ];
 
 
-constructor() public {
-initGame();
-owner = msg.sender;
-}
+    function Gobang() public {
+        //    constructor() public {
+        initGame();
+        owner = msg.sender;
+    }
 
-function joinGame() public payable {
-require(playerBlack.id == 0x0 || playerWhite.id == 0x0);
-require(msg.value >= BET_MONEY);
-if (playerBlack.id == 0x0) {
-playerBlack.id = msg.sender;
-} else {
-playerWhite.id = msg.sender;
-}
-}
+    function joinGame() public payable {
+        require(playerBlack.id == 0x0 || playerWhite.id == 0x0);
+        require(msg.value >= BET_MONEY);
+        if (playerBlack.id == 0x0) {
+            playerBlack.id = msg.sender;
+        }
+        else {
+            playerWhite.id = msg.sender;
+        }
+        if (playerBlack.id != 0x0 && playerWhite.id != 0x0) {
+            isPlaying = true;
+        }
+    }
 
-function oneStep(int8 x, int8 y) public returns (address) {
-bool isBlackStep = isBlackPlay && playerBlack.id == msg.sender;
-bool isWhiteStep = !isBlackPlay && playerWhite.id == msg.sender;
-require(chessboard[uint(x)][uint(y)] == 0 && (isBlackStep || isWhiteStep));
+    function oneStep(int8 x, int8 y) public returns (address) {
+        bool isBlackStep = isBlackPlay && playerBlack.id == msg.sender;
+        bool isWhiteStep = !isBlackPlay && playerWhite.id == msg.sender;
+        require(chessboard[uint(x)][uint(y)] == 0 && (isBlackStep || isWhiteStep));
 
-if (isBlackStep) {
-chessboard[uint(x)][uint(y)] = 1;
-playerBlack.lastX = x;
-playerBlack.lastY = y;
-}
-else if (isWhiteStep) {
-chessboard[uint(x)][uint(y)] = 2;
-playerWhite.lastX = x;
-playerWhite.lastY = y;
-}
+        if (isBlackStep) {
+            chessboard[uint(x)][uint(y)] = 1;
+            playerBlack.lastX = x;
+            playerBlack.lastY = y;
+        }
+        else if (isWhiteStep) {
+            chessboard[uint(x)][uint(y)] = 2;
+            playerWhite.lastX = x;
+            playerWhite.lastY = y;
+        }
 
-bool isWin = checkWinner(x, y);
-if (isWin) {
-if (isBlackPlay) {
-winner = playerBlack.id;
-playersMoney[playerWhite.id] -= BET_MONEY;
-playersMoney[winner] += BET_MONEY;
-} else {
-winner = playerWhite.id;
-playersMoney[playerBlack.id] -= BET_MONEY;
-playersMoney[winner] += BET_MONEY;
-}
-initGame();
-return winner;
-}
-isBlackPlay = !isBlackPlay;
-return 0x0;
-}
+        bool isWin = checkWinner(x, y);
+        if (isWin) {
+            if (isBlackPlay) {
+                winner = playerBlack.id;
+                playersMoney[playerWhite.id] -= BET_MONEY;
+                playersMoney[winner] += BET_MONEY;
+            }
+            else {
+                winner = playerWhite.id;
+                playersMoney[playerBlack.id] -= BET_MONEY;
+                playersMoney[winner] += BET_MONEY;
+            }
+            initGame();
+            return winner;
+        }
+        isBlackPlay = !isBlackPlay;
+        return 0x0;
+    }
 
-function checkWinner(int8 x, int8 y) internal view returns (bool) {
-uint8 max = 0;
-int8 tempX = x;
-int8 tempY = y;
-for (uint i = 0; i < 4; i++) {
-uint8 count = 1;
-//j为0,1分别为棋子的两边方向，比如对于横向的时候，j=0,表示下棋位子的左边，j=1的时候表示右边
-for (uint j = 0; j < 2; j++) {
-bool flag = true;
-// while语句中为一直向某一个方向遍历
-// 有相同颜色的棋子的时候，Count++
-// 否则置flag为false，结束该该方向的遍历
-while (flag) {
-tempX = tempX + dir[i][j][0];
-tempY = tempY + dir[i][j][1];
-if (chessboard[uint(tempX)][uint(tempY)] == chessboard[uint(x)][uint(y)]
-&& tempX>=0 &&tempX<15 && tempY>=0 && tempY<15) {
-count++;
-} else{
-flag = false;
-}
-}
-tempX = x;
-tempY = y;
-}
-if (count >= 5) {
-max = 1;
-break;
-} else {
-max = 0;
-}
-}
-return max == 1;
-}
+    function checkWinner(int8 x, int8 y) internal view returns (bool) {
+        uint8 max = 0;
+        int8 tempX = x;
+        int8 tempY = y;
+        for (uint i = 0; i < 4; i++) {
+            uint8 count = 1;
+            //j为0,1分别为棋子的两边方向，比如对于横向的时候，j=0,表示下棋位子的左边，j=1的时候表示右边
+            for (uint j = 0; j < 2; j++) {
+                bool flag = true;
+                // while语句中为一直向某一个方向遍历
+                // 有相同颜色的棋子的时候，Count++
+                // 否则置flag为false，结束该该方向的遍历
+                while (flag) {
+                    tempX = tempX + dir[i][j][0];
+                    tempY = tempY + dir[i][j][1];
+                    if (chessboard[uint(tempX)][uint(tempY)] == chessboard[uint(x)][uint(y)]
+                    && tempX >= 0 && tempX < 15 && tempY >= 0 && tempY < 15) {
+                        count++;
+                    }
+                    else {
+                        flag = false;
+                    }
+                }
+                tempX = x;
+                tempY = y;
+            }
+            if (count >= 5) {
+                max = 1;
+                break;
+            }
+            else {
+                max = 0;
+            }
+        }
+        return max == 1;
+    }
 
-function getNewestState() public view returns (bool, int8, int8, int8, int8) {
-return (isBlackPlay, playerBlack.lastX, playerBlack.lastY, playerWhite.lastX, playerWhite.lastY);
-}
+    function getNewestState() public view returns (bool, bool, int8, int8, int8, int8) {
+        return (isPlaying, isBlackPlay, playerBlack.lastX, playerBlack.lastY, playerWhite.lastX, playerWhite.lastY);
+    }
 
-function getMyMoney() public {
-uint senderMoney = playersMoney[msg.sender];
-require(senderMoney > 0 && address(this).balance >= senderMoney && playerBlack.id == 0x0);
-address(msg.sender).transfer(senderMoney);
-}
+    function getMyMoney() public {
+        uint senderMoney = playersMoney[msg.sender];
+        require(senderMoney > 0 && address(this).balance >= senderMoney && playerBlack.id == 0x0);
+        address(msg.sender).transfer(senderMoney);
+    }
 
-function initGame() private {
-playerBlack = Player(0x0, -1, -1);
-playerWhite = Player(0x0, -1, -1);
-isBlackPlay = true;
-winner = 0x0;
-}
+    function initGame() private {
+        playerBlack = Player(0x0, - 1, - 1);
+        playerWhite = Player(0x0, - 1, - 1);
+        isBlackPlay = true;
+        isPlaying = false;
+        winner = 0x0;
+    }
 }
